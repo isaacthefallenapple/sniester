0e388a9bf21f6e315e5f9dd1fb5fb9f8bed40759
isaacthefallenapple <isaacthefallenapple@gmail.com>
2024-05-21 11:48:45 +0200

    first

---
+++ 2024/data/parsedata.py
@@ -0,0 +1,79 @@
+import json
+import sys
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any
+
+
+@dataclass
+class Event:
+    band: str
+    venue: str
+    starttime: datetime
+    endtime: datetime
+
+
+def timestamp(timestr: str, day: int) -> datetime:
+    day = day if timestr >= "1300" else day + 1
+    return datetime.fromisoformat(f"2024-05-{day}T{formattimestr(timestr)}+02:00")
+
+
+def parse_venue(s: str) -> str:
+    return s.removesuffix(":")
+
+
+def parse_event(s: str, venue: str, day: int) -> Event:
+    print("event:", s)
+    band, timeslot = s.split(" @ ")
+    start, end = timeslot.split("-")
+    return Event(
+        band=band,
+        venue=venue,
+        starttime=timestamp(start, day=day),
+        endtime=timestamp(end, day=day),
+    )
+
+
+def parse_file(filename: str) -> list[Event]:
+    events = []
+    with open(filename, "r") as f:
+        day = next(f).strip()
+        day = int(day)
+        venue = "?"
+        for line in f:
+            line = line.strip()
+            if line == "":
+                continue
+            if line.endswith(":"):
+                venue = parse_venue(line)
+                continue
+            event = parse_event(line, day=day, venue=venue)
+            events.append(event)
+
+    return events
+
+
+def formattimestr(timestr: str) -> str:
+    return f"{timestr[:2]}:{timestr[2:]}"
+
+
+class EventEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, Event):
+            return {
+                "venue": o.venue,
+                "band": o.band,
+                "starttime": o.starttime.isoformat(),
+                "endtime": o.endtime.isoformat(),
+            }
+
+        return super().default(o)
+
+
+if __name__ == "__main__":
+    infile, outfile = sys.argv[1:]
+    print("Parsing", infile)
+    events = parse_file(infile)
+    print(events)
+    with open(outfile, "w+") as f:
+        print(json.dump(events, fp=f, cls=EventEncoder))
