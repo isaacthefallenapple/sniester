c50d00f050b5314210c466638df6447fddbdf870
isaacthefallenapple <isaacthefallenapple@gmail.com>
2025-05-15 00:21:02 +0200

    build for prod

--- 2024/build.sh
+++ 2024/build.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-elm make  --output=dist/main.js src/Main.elm
+elm make --optimize --output=dist/main.js src/Main.elm
 
 uglifyjs dist/main.js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output dist/main.min.js
